012c67c8c17a01badc79866a83af4c04dbbca34f
kesharir <ritanshu.keshari@gmail.com>
2023-07-01 19:55:37 +0530

    Adding Greet Code

--- src/main/java/com/scaler/Main.java
+++ src/main/java/com/scaler/Main.java
@@ -1,7 +1,17 @@
 package com.scaler;
 
+import com.scaler.greetings.Greeting;
+
 public class Main {
     public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+        System.out.println(greeting.greet());
         System.out.println("Hello world!");
     }
+    /**
+     * (base) 88665a0eac26:java-scaler-01 ritanshk$ java -jar build/libs/java-scaler-01-1.0-SNAPSHOT.jar
+     * Picked up JAVA_TOOL_OPTIONS: -Dlog4j2.formatMsgNoLookups=true
+     * Good Evening
+     * Hello world!
+     */
 }
---
+++ src/main/java/com/scaler/greetings/Greeting.java
@@ -0,0 +1,16 @@
+package com.scaler.greetings;
+
+public class Greeting {
+    public String greet() {
+        int hour = java.time.LocalTime.now().getHour();
+        if (hour==0 && hour<12) {
+            return "Good Morning";
+        } else if (hour>12 && hour<16) {
+            return "Good Afternoon";
+        } else if (hour>16 && hour<20) {
+          return "Good Evening";
+        } else {
+            return "Good Night";
+        }
+    }
+}
