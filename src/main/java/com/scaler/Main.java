5946ad9103da8da00d9e8f90d26cf9073a63b39e
kesharir <ritanshu.keshari@gmail.com>
2023-07-01 20:05:49 +0530

    Adding Fat Jar issue

--- src/main/java/com/scaler/Main.java
+++ src/main/java/com/scaler/Main.java
@@ -14,9 +14,28 @@ public class Main {
         System.out.println("Data : " + data);
     }
     /**
-     * (base) 88665a0eac26:java-scaler-01 ritanshk$ java -jar build/libs/java-scaler-01-1.0-SNAPSHOT.jar
+     * java -jar build/libs/java-scaler-01-1.0-SNAPSHOT.jar
+     *
      * Picked up JAVA_TOOL_OPTIONS: -Dlog4j2.formatMsgNoLookups=true
      * Good Evening
      * Hello world!
+     */
+
+    /**
+     * Fat Jar Issue:
+     *
+     * java -jar build/libs/java-scaler-01-1.0-SNAPSHOT.jar
+     *
+     * Picked up JAVA_TOOL_OPTIONS: -Dlog4j2.formatMsgNoLookups=true
+     * Good Night
+     * Hello world!
+     * Exception in thread "main" java.lang.NoClassDefFoundError: okhttp3/OkHttpClient
+     * 	at com.scaler.Client.<init>(Client.java:12)
+     * 	at com.scaler.Main.main(Main.java:12)
+     * Caused by: java.lang.ClassNotFoundException: okhttp3.OkHttpClient
+     * 	at java.base/jdk.internal.loader.BuiltinClassLoader.loadClass(BuiltinClassLoader.java:606)
+     * 	at java.base/jdk.internal.loader.ClassLoaders$AppClassLoader.loadClass(ClassLoaders.java:168)
+     * 	at java.base/java.lang.ClassLoader.loadClass(ClassLoader.java:522)
+     * 	... 2 more
      */
 }
